9cf52891e4e7a209155e0d290a3b4e353d17eeff
João Vitor <vitorjoao.jv626@gmail.com>
2026-02-07 14:32:06 -0300

    Script.js add for event list in surprise button sucess heahhh!

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('buttonsurprise');
+
+function alertbutton(){
+  alert("Hello World, it´s ok code!");
+  console.log("Surprise button click sucess.")
+}
+
+button.addEventListener('click', alertbutton);
