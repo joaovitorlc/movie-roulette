c016a8be14f966543f6c5434e8cb624a1801c570
João Vitor <vitorjoao.jv626@gmail.com>
2026-02-08 18:25:19 -0300

    feat: display movie poster and overview from API

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const button = document.getElementById('buttonsurprise');
 const display = document.getElementById('result');
-const movies = ["Matrix", "Interstellar", "O Poderoso Chefão"];
+const API_KEY = 'e004b2fab558d15191ae4ab97b1c6599';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/popular';
 
-function surprisemovie() {
+async function surprisemovie() {
+  const response = await fetch(`${BASE_URL}?api_key=${API_KEY}&page=1`);
+
+  const info = await response.json();
+
+  const movies = info.results;
+
   const randomnumber = Math.floor(Math.random() * movies.length);
   const chosenmovie = movies[randomnumber];
-  display.innerText = "Your should watch: " + chosenmovie;
+  display.innerText = "Your should watch: " + chosenmovie.title;
+  document.getElementById('overview').innerText = chosenmovie.overview;
+
+  const image = document.getElementById('moviephoto');
+  const pathphoto = "https://image.tmdb.org/t/p/w500" + chosenmovie.poster_path;
+  
+  image.src = pathphoto;
+  image.style.display="block";
 }
 
 button.addEventListener('click', surprisemovie);
